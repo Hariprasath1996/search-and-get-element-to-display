b4b1b735153c30079edd6cb4465fd956e4e8a4ac
HariPrasath1996 <hasathphy96@gmail.com>
2024-05-28 20:47:21 +0530

    completed practice

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ console.log(elements);
 function check(event) {
     enteredText = event.target.value;
     for (i = 0; i <= elements.length; i++) {
-        if (elements[0].textContent.indexOf(enteredText) < 0) {
-            elements[0].style.display = "none"
+        if (elements[i].textContent.indexOf(enteredText) < 0) {
+            elements[i].style.display = "none"
         }
         else {
-            elements[0].style.display = "block"
+            elements[i].style.display = "block"
         }
 
 
