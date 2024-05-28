e15c98e76d9be48b99cc371b7896f8ed392bede5
HariPrasath1996 <hasathphy96@gmail.com>
2024-05-28 20:45:40 +0530

    created logics

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ let enteredText = ""
 
 // select elements 
 
-let container = document.querySelector(".div")
-let elements = container.querySelectorAll(".h1")
-console.log("hello");
+let container = document.querySelector("div")
+let elements = container.querySelectorAll("h1")
+console.log(elements);
+
+function check(event) {
+    enteredText = event.target.value;
+    for (i = 0; i <= elements.length; i++) {
+        if (elements[0].textContent.indexOf(enteredText) < 0) {
+            elements[0].style.display = "none"
+        }
+        else {
+            elements[0].style.display = "block"
+        }
+
+
+    }
+}
